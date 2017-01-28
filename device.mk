c1eed3bba948a5e4711de2e3e17abf56f3d39e7b
Michael Ott <m@ott.cx>
2017-01-28 21:12:29 +0100

    Use gps stack from https://github.com/CyanogenMod/android_device_asus_msm8916-common This fixes problems with the introduction of GnssSvStatus in Android N by renaming it to QcomSvStatus. Also requires patching of libloc_api_v02.so and liblbs_core.so: sed -i 's/GnssSvStatus/QcomSvStatus/g'

--- device.mk
+++ device.mk
@@ -76,14 +76,12 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    gps.msm8916
+    gps.msm8916 \
+    flp.conf \
+    gps.conf \
+    izat.conf \
+    sap.conf
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
 # Lights
 PRODUCT_PACKAGES += \
